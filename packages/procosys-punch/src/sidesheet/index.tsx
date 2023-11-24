c52cb35f748d5322eebeeed0c1a9c1f904e1187d
Anne Skjæveland <anne.skjeveland@bouvet.no>
2023-11-24 09:15:34 +0100

    added eds to project

--- packages/procosys-punch/src/sidesheet/index.tsx
+++ packages/procosys-punch/src/sidesheet/index.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@equinor/eds-core-react";
+import { Button, Tabs } from "@equinor/eds-core-react";
 import { SideSheet } from "@equinor/fusion-react-side-sheet";
 
 import { useState } from "react";
 
 export default function PunchSideSheet() {
   const [openSheet, setOpenSheet] = useState(false);
+  const [activeTab, setActiveTab] = useState(0);
   return (
     <div>
       <Button onClick={() => setOpenSheet(true)}>Open Side Sheet</Button>
@@ -14,6 +15,7 @@ export default function PunchSideSheet() {
         onClose={() => setOpenSheet(false)}
         isOpen={openSheet}
       >
+        <SideSheet.Indicator />
         <SideSheet.Title title={"test"} />
         <SideSheet.SubTitle subTitle="test1" />
         <SideSheet.Actions>
@@ -22,7 +24,22 @@ export default function PunchSideSheet() {
             variant="ghost_icon"
           ></Button>
         </SideSheet.Actions>
-        <SideSheet.Content />
+        <SideSheet.Content>
+          <Tabs
+            onChange={(index) => setActiveTab(index)}
+            activeTab={activeTab}
+            variant="fullWidth"
+          >
+            <Tabs.List>
+              <Tabs.Tab value={0}>Main</Tabs.Tab>
+              <Tabs.Tab value={1}>History</Tabs.Tab>
+            </Tabs.List>
+            <Tabs.Panels>
+              <Tabs.Panel>Panel one</Tabs.Panel>
+              <Tabs.Panel>Panel two</Tabs.Panel>
+            </Tabs.Panels>
+          </Tabs>
+        </SideSheet.Content>
       </SideSheet>
     </div>
   );
